51c302ff0cb2a3d270717655ba1157d1a1689138
nanophate <nanophate@me.com>
2016-05-23 17:29:07 +0900

    Add blog page for website

---
+++ app/controllers/blog_post_page_controller.rb
@@ -0,0 +1,2 @@
+class BlogPostPageController < CmsController
+end
---
+++ app/models/blog_page.rb
@@ -0,0 +1,5 @@
+class BlogPage < Obj
+  attribute :title, :string
+  attribute :body, :widgetlist
+  attribute :child_order, :referencelist
+end
