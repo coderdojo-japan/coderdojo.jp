f637ae829efe7a5b24ec7e04410df3ee1164149f
nanophate <nanophate@me.com>
2016-06-14 19:38:31 +0900

    Add test for coder dojo model

--- app/models/coder_dojo.rb
+++ app/models/coder_dojo.rb
@@ -1,2 +1,3 @@
 class CoderDojo < ActiveRecord::Base
+  validates :name, presence: true
 end
--- test/models/coder_dojo_test.rb
+++ test/models/coder_dojo_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class CoderDojoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @dojo = CoderDojo.new(name: "Example User", 
+                     course: "scratch", 
+                     caption: "Weekly Event", 
+                     venue: "okinawa", 
+                     region: '47', 
+                     logo_image_url: "https://www.example.com/logo.png", 
+                     redirect_url: "https://www.facebook.com", 
+                     user_name: "example", 
+                     email: "user@example.com")
+  end
+  
+  test "should be valid" do
+  	assert @dojo.valid?
+  end
+
+   test "name should be present" do
+    @dojo.name = "     "
+    assert_not @dojo.valid?
+  end
+  
 end
