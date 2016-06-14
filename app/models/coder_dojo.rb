7395a3ca54e80638fdf233ad47158f1503322bae
nanophate <nanophate@me.com>
2016-06-14 19:55:35 +0900

    Add test for each col

--- app/models/coder_dojo.rb
+++ app/models/coder_dojo.rb
@@ -1,3 +1,11 @@
 class CoderDojo < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, 			  presence: true
+  validates :course, 		  presence: true
+  validates :caption, 		  presence: true
+  validates :venue, 		  presence: true
+  validates :region, 		  presence: true
+  validates :logo_image_url,  presence: true
+  validates :redirect_url,    presence: true
+  validates :user_name, 	  presence: true
+  validates :email,           presence: true
 end
--- test/models/coder_dojo_test.rb
+++ test/models/coder_dojo_test.rb
@@ -3,23 +3,58 @@ require 'test_helper'
 class CoderDojoTest < ActiveSupport::TestCase
   def setup
     @dojo = CoderDojo.new(name: "Example User", 
-                     course: "scratch", 
-                     caption: "Weekly Event", 
-                     venue: "okinawa", 
-                     region: '47', 
-                     logo_image_url: "https://www.example.com/logo.png", 
-                     redirect_url: "https://www.facebook.com", 
-                     user_name: "example", 
-                     email: "user@example.com")
+                          course: "scratch", 
+                          caption: "Weekly Event", 
+                          venue: "okinawa", 
+                          region: '47', 
+                          logo_image_url: "https://www.example.com/logo.png", 
+                          redirect_url: "https://www.facebook.com", 
+                          user_name: "example", 
+                          email: "user@example.com")
   end
   
   test "should be valid" do
-  	assert @dojo.valid?
+    assert @dojo.valid?
   end
 
-   test "name should be present" do
-    @dojo.name = "     "
+  test "name should be present" do
+    @dojo.name  = "    "
     assert_not @dojo.valid?
   end
   
+  test "course should be present" do
+  	@dojo.course = "    "
+  	assert_not @dojo.valid?
+  end
+
+  test "caption should be present" do
+  	@dojo.caption = "    "
+  	assert_not @dojo.valid?
+  end
+
+  test "venue should be present" do
+  	@dojo.venue = "    "
+  	assert_not @dojo.valid?
+  end
+
+  test "region should be present" do
+  	@dojo.region = '    '
+  	assert_not @dojo.valid?
+  end
+  
+  test "logo should be present" do
+  	@dojo.logo_image_url = "    "
+  	assert_not @dojo.valid?
+  end
+
+  test "dojo url should be present" do
+  	@dojo.redirect_url = "    "
+  	assert_not @dojo.valid?
+  end
+
+  test "user name should be present" do
+  	@dojo.user_name = "    "
+  	assert_not @dojo.valid?
+  end
+
 end
