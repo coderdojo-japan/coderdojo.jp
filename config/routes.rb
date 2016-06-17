40b4eeb415c84908a0f2a52ab2583a37e1ead9f6
nanophate <nanophate@me.com>
2016-06-17 17:08:22 +0900

    Add form to gather informtaion

--- config/routes.rb
+++ config/routes.rb
@@ -59,4 +59,5 @@ Rails.application.routes.draw do
   scrivito_route '/', using: 'homepage'
   scrivito_route '(/)(*slug-):id', using: 'slug_id'
   scrivito_route '/*permalink', using: 'permalink', format: false
+
 end
