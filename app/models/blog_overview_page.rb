3bd4cb95f92fa98b03946506d5f5d81b528dc11e
nanophate <nanophate@me.com>
2016-10-24 09:52:29 +0900

    Fix image not being displayed on blog and blogOverview

--- app/models/blog_overview_page.rb
+++ app/models/blog_overview_page.rb
@@ -1,6 +1,5 @@
 class BlogOverviewPage < Obj
   attribute :title, :string
-  attribute :image, :reference
   attribute :body, :widgetlist
   attribute :child_order, :referencelist
 end
--- app/models/blog_post_page.rb
+++ app/models/blog_post_page.rb
@@ -1,7 +1,9 @@
 class BlogPostPage < Obj
   attribute :title, :string
+  attribute :image, :reference
   attribute :body, :widgetlist
   attribute :created, :date
+  attribute :abstract, :html
 
   default_for :created do
     Time.zone.now
