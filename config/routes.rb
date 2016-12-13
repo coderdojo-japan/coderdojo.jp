0f3703d19c275036ca87ff9f3cd8b6cd6b1617b6
Yohei YASUKAWA <yasulab@gmail.com>
2016-12-13 10:12:48 +0900

    Use /blogs instead of /releases

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/sotechsha',       to: 'so_tech_sha_overview_page#index'
   get '/sotechsha/:quiz', to: 'so_tech_sha#quiz'
 
+  # Redirects
+  get "/releases/2016/12/12/new-backend", to: redirect('/blogs/2016/12/12/new-backend')
   # Default Scrivito routes. Adapt them to change the routing of CMS objects.
   # See the documentation of 'scrivito_route' for a detailed description.
   scrivito_route '/',              using: 'homepage'
