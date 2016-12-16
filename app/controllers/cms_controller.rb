5fd1cf19db210b5a9600ce3b47de752f314256c3
Yohei YASUKAWA <yasulab@gmail.com>
2016-12-16 11:20:00 +0900

    Move Shared Workspace to CmsController

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
 class CmsController < ApplicationController
   include Scrivito::ControllerActions
+
+  Scrivito::Workspace.use(ENV["SCRIVITO_WORKSPACE"])
+  LoginPage.create(title: 'ログイン')
 end
