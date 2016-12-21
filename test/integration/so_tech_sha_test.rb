5c56121e316b5449445bff3e4fe2a3efd72b2d6b
tomoko <t0m0k05238188+mac@gmail.com>
2016-12-21 18:28:25 +0900

    Add snsbtn to toppage
    ・delete snsbtn from blogpostpage
    ・fix support url
    ・delete closing sitemap

--- test/integration/so_tech_sha_test.rb
+++ test/integration/so_tech_sha_test.rb
@@ -30,9 +30,10 @@ class SoTechShaTest < ActionDispatch::IntegrationTest
     get "/sotechsha"
     assert_template "so_tech_sha_overview_page/index"
     assert_select "h1.page-header","「Scratchでつくる! たのしむ! プログラミング道場」Webコンテンツ"
-    assert_select "a[href]", count:21
+    assert_select "a[href]", count:23
     assert_select "footer", count:1
-    assert_select "img", count:1
+    # topimg,snsbtn
+    assert_select "img", count:2
   end
 
   test "Datetime should be formatted" do
