4cd1a964013379d9b3b255a145d22eea86cbca46
tomoko <t0m0k05238188+mac@gmail.com>
2016-12-22 12:47:49 +0900

    Partial page of sitemap is not displayed when sts_posts is nil

--- test/integration/blog_test.rb
+++ test/integration/blog_test.rb
@@ -8,6 +8,8 @@ class BlogTest < ActionDispatch::IntegrationTest
   test "Blog post should be rendered" do
     get "/blogs/2016/12/12/new-backend"
     assert_template "blog_post_page/index"
+    assert_template partial: '_sitemap_sotechsha', count: 0
+    assert_template partial: '_footer', count: 1
     assert_select "title", "CoderDojo Japan のバックエンドを更新しました - CoderDojo Japan"
   end
 end
