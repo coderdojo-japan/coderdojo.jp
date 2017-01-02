1f690e7b2d06429d73ab16a6d146b686ad0c9983
Yohei Yasukawa <yasulab@gmail.com>
2017-01-02 12:30:45 +0900

    Add 'data:' and other missing domains to SecureHeaders

--- config/initializers/csp.rb
+++ config/initializers/csp.rb
@@ -3,11 +3,10 @@ SecureHeaders::Configuration.default do |config|
     report_only: Rails.env.production?, # default: false
     preserve_schemes: true,             # default: false.
 
-    default_src: %w(* 'unsafe-inline' 'unsafe-eval'
+    default_src: %w(* 'unsafe-inline' 'unsafe-eval' data:
 *.dropboxusercontent.com
-*.google-analytics.com *.googleapis.com *.google.com
-*.facebook.net *.facebook.com
-*.twitter.com
+*.google-analytics.com *.googleapis.com *.google.com *.gstatic.com
+*.facebook.net *.facebook.com *.twitter.com
 *.hatena.ne.jp *.st-hatena.com
 *.slidesharecdn.com *.slideshare.net),
     report_uri: ["/csp_report?report_only=#{Rails.env.production?}"]
