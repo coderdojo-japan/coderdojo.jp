bc556b2e7dad9a22cb14fdb395efe67b7513b6d5
Yohei Yasukawa <yasulab@gmail.com>
2017-01-05 19:35:34 +0900

    Refactor: Use hash rocket abbr and Rubyish String comparison

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :store_location , :unless=> :login_page_access?
+  before_action :store_location , unless: :login_page_access?
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   include SessionsHelper
-  
+
   private
 
     def store_location
@@ -14,7 +13,7 @@ class ApplicationController < ActionController::Base
     end
 
     def login_page_access?
-      self.controller_name == 'login_page' || self.controller_name =='sessions'
+      %w(login_page sessions).include? self.controller_name
     end
 
 end
