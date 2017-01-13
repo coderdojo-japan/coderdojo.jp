0ec224234d4f68abbb9baaa538e7a7af4c053d14
Yohei Yasukawa <yasulab@gmail.com>
2017-01-13 13:06:40 +0900

    Fix failed tests due to typo: sponser

--- test/integration/top_test.rb
+++ test/integration/top_test.rb
@@ -2,8 +2,8 @@
 require 'test_helper'
 
 class Toptest < ActionDispatch::IntegrationTest
-  test "Sponserd link should be exist" do
+  test "Sponser links should be exist" do
       get "/"
-      assert_select 'section.sponsers_logo a[href]',count:4
+      assert_select 'section.sponsors_logo a[href]', count:4
   end
 end
