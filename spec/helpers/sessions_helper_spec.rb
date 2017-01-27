b829e3b3516f658e479409ae1ba73d4d2af4dc47
Yohei Yasukawa <yasulab@gmail.com>
2017-01-27 19:18:18 +0900

    Comment out needless helper specs

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -10,6 +10,7 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe SessionsHelper, :type => :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
+#RSpec.describe SessionsHelper, :type => :helper do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
