c85a38ee6b75d8929a1fb026db6938813b1cabe3
Yohei Yasukawa <yasulab@gmail.com>
2017-02-16 19:17:53 +0900

    Add specs for Dojo model validations

---
+++ spec/models/dojo_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe Dojo, :type => :model do
+  before do
+    @dojo = Dojo.new(name:  "CoderDojo 下北沢", email: "shimokitazawa@coderdojo.jp",
+                 order: 0, description: "東京都世田谷区で毎週開催",
+                 logo: "https://graph.facebook.com/346407898743580/picture?type=large",
+                 url:  "http://tokyo.coderdojo.jp/",
+                 tags: ["Scratch", "Webサイト", "ゲーム"])
+  end
+
+  subject { @dojo }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:order) }
+  it { should respond_to(:description) }
+  it { should respond_to(:logo) }
+  it { should respond_to(:url) }
+  it { should respond_to(:tags) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @dojo.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @dojo.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+    before { @dojo.email = " " }
+    it { should be_valid }
+  end
+
+  describe "when description is not present" do
+    before { @dojo.description = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when description is too long" do
+    before { @dojo.description = "a" * 51 }
+    it { should_not be_valid }
+  end
+
+  describe 'when tags is not present' do
+    before { @dojo.tags = [] }
+    it { should_not be_valid }
+  end
+
+  describe 'when tags are too many' do
+    before { @dojo.tags = %w[Scratch RasPi HackforPlay Viscuit Arduino 電子工作] }
+    it { should_not be_valid }
+  end
+
+end
