345562dce7d5b917ba78e348a967a06be12eb49e
Yohei Yasukawa <yasulab@gmail.com>
2017-02-16 19:55:33 +0900

    Add specs for yaml format validtor and for dojos.yaml

--- spec/models/dojo_spec.rb
+++ spec/models/dojo_spec.rb
@@ -56,4 +56,21 @@ RSpec.describe Dojo, :type => :model do
     it { should_not be_valid }
   end
 
+  describe 'yaml format validator' do
+    let(:valid_yaml) {      Rails.root.join('spec', 'data',   'valid_format_of.yaml') }
+    let(:invalid_yaml) {    Rails.root.join('spec', 'data', 'invalid_format_of.yaml') }
+    let(:production_yaml) { Rails.root.join('db','dojos.yaml')}
+
+    it 'should return true when valid yaml format' do
+      expect(Dojo.valid_yaml_format? valid_yaml).to be true
+    end
+
+    it 'should return false when invalid yaml format' do
+      expect(Dojo.valid_yaml_format? invalid_yaml).to be false
+    end
+
+    it 'should always return true for production yaml file' do
+      expect(Dojo.valid_yaml_format? production_yaml).to be true
+    end
+  end
 end
