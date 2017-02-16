315203d032c732869f7a6aeeaf280c07666baa4b
Yohei Yasukawa <yasulab@gmail.com>
2017-02-16 18:36:26 +0900

    Add validations to Dojo model

--- app/models/dojo.rb
+++ app/models/dojo.rb
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 class Dojo < ApplicationRecord
   NUM_OF_COUNTRIES   = "70"
   NUM_OF_WHOLE_DOJOS = "1,200"
@@ -7,4 +6,14 @@ class Dojo < ApplicationRecord
 
   serialize :tags
   default_scope -> { order(order: :asc) }
+  before_save { self.email = self.email.downcase }
+
+  validates :name,        presence: true, length: { maximum: 50 }
+  validates :email,       presence: false
+  validates :order,       presence: false
+  validates :description, presence: true, length: { maximum: 50 }
+  validates :logo,        presence: false
+  validates :tags,        presence: true
+  validates :url,         presence: true
+
 end
