d6c55c53f40f07398043e3891bef935594a54305
Yohei Yasukawa <yasulab@gmail.com>
2017-03-14 15:45:35 +0900

    Fix broken spec in Top

--- spec/features/top_spec.rb
+++ spec/features/top_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Top", type: :feature do
   describe "GET /" do
     scenario "Sponser links should be exist" do
       visit "/"
-      expect(page).to have_css 'section.sponsors_logo a[href]', count: 5
+      expect(page).to have_css 'section.sponsors_logo a[href]', count: 6
     end
   end
 end
