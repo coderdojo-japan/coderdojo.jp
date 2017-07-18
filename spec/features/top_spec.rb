e074fcfc86d43125361ec2035a7424375b31c924
Yohei Yasukawa <yasulab@gmail.com>
2017-07-18 14:23:00 +0900

    Fix test the number of sponsors

--- spec/features/top_spec.rb
+++ spec/features/top_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Top", type: :feature do
   describe "GET /" do
     scenario "Sponser links should be exist" do
       visit "/"
-      expect(page).to have_css 'section.sponsors_logo a[href]', count: 6
+      expect(page).to have_css 'section.sponsors_logo a[href]', count: 7
     end
   end
 end
