32315e8c4a65283679d35277f65ebe7c41364ce7
Yohei Yasukawa <yasulab@gmail.com>
2017-10-29 13:41:53 +0900

    Refactor: Tweak newlines in SotechshaOverviewPageController

--- app/controllers/sotechsha_overview_page_controller.rb
+++ app/controllers/sotechsha_overview_page_controller.rb
@@ -1,7 +1,6 @@
 class SotechshaOverviewPageController < ScrivitoController
-
   def index
-    @sts_posts = BlogPostPage.where(:_permalink, :starts_with, 'sotechsha/').order(created: :asc)
+    @sts_posts = BlogPostPage.where(:_permalink, :starts_with,
+                                    'sotechsha/').order(created: :asc)
   end
-
 end
