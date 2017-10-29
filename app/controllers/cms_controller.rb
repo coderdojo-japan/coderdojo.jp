b3778775bb70739af85a86e096ec3f1d9019ba16
Yohei Yasukawa <yasulab@gmail.com>
2017-10-29 09:37:15 +0900

    Don't require SCRIVITO_WORKSPACE to boot

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -2,6 +2,9 @@
 class CmsController < ApplicationController
   include Scrivito::ControllerActions
 
-  Scrivito::Workspace.use(ENV["SCRIVITO_WORKSPACE"])
+  title ||= ENV['SCRIVITO_WORKSPACE'] || 'DEFAULT_WORKSPACE'
+  Scrivito::Workspace.create(title: title) unless Scrivito::Workspace.find_by_title(title)
+  Scrivito::Workspace.use(title)
+
   LoginPage.create(title: 'ログイン')
 end
