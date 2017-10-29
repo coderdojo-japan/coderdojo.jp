2c47b5674d37df77a92be6cd014927c56d51c428
Yohei Yasukawa <yasulab@gmail.com>
2017-10-29 12:00:29 +0900

    Enable to override meta tags via helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,28 @@
 # -*- coding: utf-8 -*-
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "CoderDojo Japan"
+    page_title ||= @obj.display_title if @obj && !@obj.permalink.nil?
     if page_title.empty?
-      "CoderDojo Japan - 子どものためのプログラミング道場"
+      "CoderDojo Japan - 子どものためのプログラミング道場" # Default title
     else
-      "#{page_title} - #{base_title}"
+      "#{page_title} - CoderDojo Japan"
     end
   end
 
   def full_url(page_url)
-    default_url = "https://coderdojo.jp/"
+    base_url = 'https://coderdojo.jp/'
+    page_url ||= base_url + @obj.permalink if @obj && !@obj.permalink.nil?
     if page_url.empty?
-      default_url
+      "https://coderdojo.jp/" # Default URL
     else
       page_url
     end
   end
 
   def meta_description(description)
+    description ||= kata_description if @obj && @obj.permalink == "kata"
     if description.empty?
-      "CoderDojo は子どものためのプログラミング道場です。2011年にアイルランドで始まり、全国では#{Dojo::NUM_OF_JAPAN_DOJOS}ヶ所以上、世界では#{Dojo::NUM_OF_COUNTRIES}ヶ国・#{Dojo::NUM_OF_WHOLE_DOJOS}ヶ所で開催されています。"
+      "CoderDojo は子どものためのプログラミング道場です。2011年にアイルランドで始まり、全国では#{Dojo::NUM_OF_JAPAN_DOJOS}ヶ所以上、世界では#{Dojo::NUM_OF_COUNTRIES}ヶ国・#{Dojo::NUM_OF_WHOLE_DOJOS}ヶ所で開催されています。" # Default description
     else
       description
     end
