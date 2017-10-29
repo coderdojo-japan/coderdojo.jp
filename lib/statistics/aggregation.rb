213320cdfaa95c923e066b53c73e88a16983c3ff
nalabjp <nalabjp@gmail.com>
2017-10-29 23:27:58 +0900

    Support facebook on Statistics::Aggregation

--- lib/statistics/aggregation.rb
+++ lib/statistics/aggregation.rb
@@ -4,9 +4,11 @@ module Statistics
       def run(date:)
         cnps_dojos = Dojo.joins(:dojo_event_service).where(dojo_event_services: { name: :connpass }).to_a
         drkp_dojos = Dojo.joins(:dojo_event_service).where(dojo_event_services: { name: :doorkeeper }).to_a
+        fsbk_dojos = Dojo.joins(:dojo_event_service).where(dojo_event_services: { name: :facebook }).to_a
 
         Connpass.run(cnps_dojos, date)
         Doorkeeper.run(drkp_dojos, date)
+        Facebook.run(fsbk_dojos, date)
       end
     end
 
@@ -57,6 +59,33 @@ module Statistics
                                    event_url: e['public_url'],
                                    participants: e['participants'],
                                    evented_at: Time.zone.parse(e['starts_at']))
+            end
+          end
+        end
+      end
+    end
+
+    class Facebook
+      class << self
+        def run(dojos, date)
+          fsbk = Client::Facebook.new
+          params = {
+            since_at: date.beginning_of_month,
+            until_at: date.end_of_month
+          }
+
+          dojos.each do |dojo|
+            fsbk.fetch_events(params.merge(group_id: dojo.dojo_event_service.group_id)).each do |e|
+              next unless e.dig('owner', 'id') == dojo.dojo_event_service.group_id
+
+              EventHistory.create!(dojo_id: dojo.id,
+                                   dojo_name: dojo.name,
+                                   service_name: dojo.dojo_event_service.name,
+                                   service_group_id: dojo.dojo_event_service.group_id,
+                                   event_id: e['id'],
+                                   event_url: "https://www.facebook.com/events/#{e['id']}",
+                                   participants: e['attending_count'],
+                                   evented_at: Time.zone.parse(e['start_time']))
             end
           end
         end
