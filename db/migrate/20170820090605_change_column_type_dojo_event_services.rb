5e5a27b751879948091d5ce284090bfae5d0bfc8
nalabjp <nalabjp@gmail.com>
2017-11-04 00:41:44 +0900

    Fix to change column type with PostgreSQL in production

--- db/migrate/20170820090605_change_column_type_dojo_event_services.rb
+++ db/migrate/20170820090605_change_column_type_dojo_event_services.rb
@@ -1,6 +1,12 @@
 class ChangeColumnTypeDojoEventServices < ActiveRecord::Migration[5.0]
-  def up
-    change_column :dojo_event_services, :name, :integer, null: false
+  if connection.adapter_name == 'PostgreSQL'
+    def up
+      change_column :dojo_event_services, :name, 'integer USING CAST(name AS integer)', null: false
+    end
+  else
+    def up
+      change_column :dojo_event_services, :name, :integer, null: false
+    end
   end
 
   def down
