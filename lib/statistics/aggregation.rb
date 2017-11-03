e86a09aa4779df7fc7d76a7d4d7f37b67b070411
nalabjp <nalabjp@gmail.com>
2017-11-04 04:08:36 +0900

    `has_one` -> `has_many` between Dojo and DojoEventService

--- lib/statistics/aggregation.rb
+++ lib/statistics/aggregation.rb
@@ -2,9 +2,9 @@ module Statistics
   class Aggregation
     class << self
       def run(date:)
-        cnps_dojos = Dojo.joins(:dojo_event_service).where(dojo_event_services: { name: :connpass }).to_a
-        drkp_dojos = Dojo.joins(:dojo_event_service).where(dojo_event_services: { name: :doorkeeper }).to_a
-        fsbk_dojos = Dojo.joins(:dojo_event_service).where(dojo_event_services: { name: :facebook }).to_a
+        cnps_dojos = Dojo.joins(:dojo_event_services).where(dojo_event_services: { name: :connpass }).to_a
+        drkp_dojos = Dojo.joins(:dojo_event_services).where(dojo_event_services: { name: :doorkeeper }).to_a
+        fsbk_dojos = Dojo.joins(:dojo_event_services).where(dojo_event_services: { name: :facebook }).to_a
 
         Connpass.run(cnps_dojos, date)
         Doorkeeper.run(drkp_dojos, date)
@@ -21,17 +21,19 @@ module Statistics
           }
 
           dojos.each do |dojo|
-            cnps.fetch_events(params.merge(series_id: dojo.dojo_event_service.group_id)).each do |e|
-              next unless e.dig('series', 'id').to_s == dojo.dojo_event_service.group_id
+            dojo.dojo_event_services.each do |dojo_event_service|
+              cnps.fetch_events(params.merge(series_id: dojo_event_service.group_id)).each do |e|
+                next unless e.dig('series', 'id').to_s == dojo_event_service.group_id
 
-              EventHistory.create!(dojo_id: dojo.id,
-                                   dojo_name: dojo.name,
-                                   service_name: dojo.dojo_event_service.name,
-                                   service_group_id: dojo.dojo_event_service.group_id,
-                                   event_id: e['event_id'],
-                                   event_url: e['event_url'],
-                                   participants: e['accepted'],
-                                   evented_at: Time.zone.parse(e['started_at']))
+                EventHistory.create!(dojo_id: dojo.id,
+                                     dojo_name: dojo.name,
+                                     service_name: dojo_event_service.name,
+                                     service_group_id: dojo_event_service.group_id,
+                                     event_id: e['event_id'],
+                                     event_url: e['event_url'],
+                                     participants: e['accepted'],
+                                     evented_at: Time.zone.parse(e['started_at']))
+              end
             end
           end
         end
@@ -48,17 +50,19 @@ module Statistics
           }
 
           dojos.each do |dojo|
-            drkp.fetch_events(params.merge(group_id: dojo.dojo_event_service.group_id)).each do |e|
-              next unless e['group'].to_s == dojo.dojo_event_service.group_id
+            dojo.dojo_event_services.each do |dojo_event_service|
+              drkp.fetch_events(params.merge(group_id: dojo_event_service.group_id)).each do |e|
+                next unless e['group'].to_s == dojo_event_service.group_id
 
-              EventHistory.create!(dojo_id: dojo.id,
-                                   dojo_name: dojo.name,
-                                   service_name: dojo.dojo_event_service.name,
-                                   service_group_id: dojo.dojo_event_service.group_id,
-                                   event_id: e['id'],
-                                   event_url: e['public_url'],
-                                   participants: e['participants'],
-                                   evented_at: Time.zone.parse(e['starts_at']))
+                EventHistory.create!(dojo_id: dojo.id,
+                                     dojo_name: dojo.name,
+                                     service_name: dojo_event_service.name,
+                                     service_group_id: dojo_event_service.group_id,
+                                     event_id: e['id'],
+                                     event_url: e['public_url'],
+                                     participants: e['participants'],
+                                     evented_at: Time.zone.parse(e['starts_at']))
+              end
             end
           end
         end
@@ -75,17 +79,19 @@ module Statistics
           }
 
           dojos.each do |dojo|
-            fsbk.fetch_events(params.merge(group_id: dojo.dojo_event_service.group_id)).each do |e|
-              next unless e.dig('owner', 'id') == dojo.dojo_event_service.group_id
+            dojo.dojo_event_services.each do |dojo_event_service|
+              fsbk.fetch_events(params.merge(group_id: dojo_event_service.group_id)).each do |e|
+                next unless e.dig('owner', 'id') == dojo_event_service.group_id
 
-              EventHistory.create!(dojo_id: dojo.id,
-                                   dojo_name: dojo.name,
-                                   service_name: dojo.dojo_event_service.name,
-                                   service_group_id: dojo.dojo_event_service.group_id,
-                                   event_id: e['id'],
-                                   event_url: "https://www.facebook.com/events/#{e['id']}",
-                                   participants: e['attending_count'],
-                                   evented_at: Time.zone.parse(e['start_time']))
+                EventHistory.create!(dojo_id: dojo.id,
+                                     dojo_name: dojo.name,
+                                     service_name: dojo_event_service.name,
+                                     service_group_id: dojo_event_service.group_id,
+                                     event_id: e['id'],
+                                     event_url: "https://www.facebook.com/events/#{e['id']}",
+                                     participants: e['attending_count'],
+                                     evented_at: Time.zone.parse(e['start_time']))
+              end
             end
           end
         end
