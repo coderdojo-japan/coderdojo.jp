764be550fc689c57edfa4404270afc9ea6485db2
Yohei Yasukawa <yasulab@gmail.com>
2017-11-08 08:47:18 +0900

    Add test to check if footer exists in Top #192

--- spec/features/top_spec.rb
+++ spec/features/top_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "Top", type: :feature do
   describe "GET /" do
-    scenario "Sponser links should be exist" do
+    scenario "Each section should exist" do
       visit "/"
-      expect(page).to have_css 'section.sponsors_logo a[href]'
+      expect(page).to have_title 'CoderDojo Japan'
+      expect(page).to have_text  '全国の道場'
+      expect(page).to have_css   'section.sponsors_logo a[href]'
+      expect(page).to have_text  '問い合わせ'
+      expect(page).to have_text  '一般社団法人'
     end
   end
 end
