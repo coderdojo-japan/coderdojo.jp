1376a20919e04671f207e656b204a0f1f43a5f69
nalabjp <nalabjp@gmail.com>
2017-11-13 01:36:34 +0900

    Use mock

--- spec/requests/news_pages_spec.rb
+++ spec/requests/news_pages_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "NewsPages", type: :request do
   describe "GET /news/2016/12/12/new-backend" do
     it "Blog post should be rendered" do
+      obj = mock_obj(Obj, permalink: "/news/2016/12/12/new-backend")
+      allow(Scrivito::BasicObj).to receive(:find_by_permalink).with("news/2016/12/12/new-backend") { obj }
       get '/news/2016/12/12/new-backend'
       expect(response).to have_http_status(200)
     end
