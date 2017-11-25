4c628e777dd3960d95c7a43e575e52ec1a130fd5
nalabjp <nalabjp@gmail.com>
2017-11-26 02:25:02 +0900

    Move Scrivito' workspace configuration to initialization

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -1,9 +1,5 @@
 class CmsController < ApplicationController
   include Scrivito::ControllerActions
 
-  title ||= ENV['SCRIVITO_WORKSPACE'] || 'DEFAULT_WORKSPACE'
-  Scrivito::Workspace.create(title: title) unless Scrivito::Workspace.find_by_title(title)
-  Scrivito::Workspace.use(title)
-
   LoginPage.create(title: 'ログイン')
 end
