c3957f9efa3729b3f765efd01ae2c3906f0889be
nalabjp <nalabjp@gmail.com>
2017-11-26 13:43:34 +0900

    Remove useless test directory

--- test/integration/top_test.rb
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-require 'test_helper'
-
-class TopTest < ActionDispatch::IntegrationTest
-  test "Sponser links should be exist" do
-      get "/"
-      assert_select 'section.sponsors_logo a[href]', count:4
-  end
-end
