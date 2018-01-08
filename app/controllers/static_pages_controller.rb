163f3a04b4647634831fe89157d32745ea8caa44
Yohei Yasukawa <yasulab@gmail.com>
2018-01-08 10:41:53 +0900

    Ignore static dojos from stats

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,13 +10,13 @@ class StaticPagesController < ApplicationController
     @regions_and_dojos   = Dojo.eager_load(:prefecture).default_order.group_by { |dojo| dojo.prefecture.region }
 
     # TODO: 次の静的なDojoの開催数もデータベース上で集計できるようにする
-    @sum_of_events       = EventHistory.count + # 以下は2017年11月3日時点で個別に確認した数字
-      29 + # 柏の葉
-      3  + # 南柏
-      4  + # 柏湘南
-      63   # 小平
-    @sum_of_dojos        = DojoEventService.count('DISTINCT dojo_id') +
-      4    # TODO: 同上。上記の道場数を静的に足しています
+    @sum_of_events       = EventHistory.count #+ # 以下は2017年11月3日時点で個別に確認した数字
+      #29 + # 柏の葉
+      #3  + # 南柏
+      #4  + # 柏湘南
+      #63   # 小平
+    @sum_of_dojos        = DojoEventService.count('DISTINCT dojo_id') #+
+      #4    # TODO: 同上。上記の道場数を静的に足しています
     @sum_of_participants = EventHistory.sum(:participants)
 
     # 2012年1月1日〜2017年12月31日までの集計結果
