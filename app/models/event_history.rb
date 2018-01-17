a6ea57932676941e10bf42f91238211fe8511f9c
nalabjp <nalabjp@gmail.com>
2018-01-18 01:47:20 +0900

    Allow null at group_id and service_group_id

--- app/models/event_history.rb
+++ app/models/event_history.rb
@@ -3,7 +3,6 @@ class EventHistory < ApplicationRecord
 
   validates :dojo_name, presence: true
   validates :service_name, presence: true, uniqueness: { scope: :event_id  }
-  validates :service_group_id, presence: true
   validates :event_id, presence: true
   validates :event_url, presence: true, uniqueness: true, allow_nil: true
   validates :participants, presence: true
