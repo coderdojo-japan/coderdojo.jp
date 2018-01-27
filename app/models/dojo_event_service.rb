116327d8824547c9e1317e57415d864f8704c7b7
nalabjp <nalabjp@gmail.com>
2018-01-27 13:32:16 +0900

    Add a validation

--- app/models/dojo_event_service.rb
+++ app/models/dojo_event_service.rb
@@ -2,5 +2,7 @@ class DojoEventService < ApplicationRecord
   belongs_to :dojo
   enum name: %i( connpass doorkeeper facebook static_yaml )
 
+  validates :name, presence: true
+
   scope :for, ->(service) { where(name: service) }
 end
