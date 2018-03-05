4b8f227d002ae0fffd3fd8326ca2e4e48964f92c
AnaTofuZ <anatofuz@gmail.com>
2018-03-05 11:09:59 +0900

    lib/statistics.rbでの `providers` の名前空間からstatisticsを外した
    `providers` の名前空間を`statistics` から外した為、require_relativeのパ
    スを修正した

--- lib/statistics.rb
+++ lib/statistics.rb
@@ -1,6 +1,6 @@
 module Statistics; end
 
 require_relative 'statistics/client'
-require_relative 'statistics/providers'
 require_relative 'statistics/tasks'
 require_relative 'statistics/aggregation'
+require_relative 'providers'
