f3418033e8646191404408a907f79b08755b9033
AnaTofuZ <anatofuz@gmail.com>
2018-03-05 11:59:59 +0900

    remove spec/controller/static_pages_controller_spec for /evens

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -9,11 +9,4 @@ RSpec.describe StaticPagesController, type: :controller do
     end
   end
 
-  describe "GET #events" do
-    it "returns http success" do
-      get :events
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
