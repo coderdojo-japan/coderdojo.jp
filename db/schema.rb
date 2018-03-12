c03a16b6e017dca480aad5873e12f7656b93a3f4
AnaTofuZ <anatofuz@gmail.com>
2018-03-12 11:59:32 +0900

    remove dojo_id for recently_events schema
    テーブルの正規化を行う為に、dojo_idなどの非正規部分を削除した
    https://github.com/coderdojo-japan/coderdojo.jp/pull/277#pullrequestreview-102871498

--- db/schema.rb
+++ db/schema.rb
@@ -62,15 +62,11 @@ ActiveRecord::Schema.define(version: 20180306024000) do
   end
 
   create_table "recently_events", force: :cascade do |t|
-    t.integer "dojo_id", null: false
-    t.string "dojo_name", null: false
-    t.string "service_name", null: false
-    t.string "service_group_id"
+    t.integer "dojo_event_service_id", null: false
     t.string "event_id", null: false
     t.string "event_url", null: false
     t.datetime "event_at", null: false
-    t.index ["dojo_id"], name: "index_recently_events_on_dojo_id"
-    t.index ["event_at", "dojo_id"], name: "index_recently_events_on_event_at_and_dojo_id"
+    t.index ["dojo_event_service_id"], name: "index_recently_events_on_dojo_event_service_id"
     t.index ["event_at"], name: "index_recently_events_on_event_at"
   end
 
