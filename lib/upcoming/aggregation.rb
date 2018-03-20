2cf308d1f47341934b42f1ded657a499dbca5e40
AnaTofuZ <anatofuz@gmail.com>
2018-03-20 14:56:53 +0900

    コピーとメソッド等の修正
    Statisticsの資産を使いたかった為にコピーして大まかなメソッドをリネームし
    た

---
+++ lib/upcoming/aggregation.rb
@@ -0,0 +1,161 @@
+module Upcoming
+  class Aggregation
+    def initialize(args)
+      @from = TimeWithZone.beginning_of_week
+      @to = TimeWithZone.end_of_month
+      @dojos = fetch_dojos
+    end
+
+    def run
+      "Upcoming::Aggregation::#{@mode.camelize}".constantize.new(@dojos, @from, @to).run
+    end
+
+    private
+
+
+    def fetch_dojos
+      {
+        externals: find_dojos_by(DojoEventService::EXTERNAL_SERVICES),
+        internals: find_dojos_by(DojoEventService::INTERNAL_SERVICES)
+      }
+    end
+
+    def find_dojos_by(services)
+      services.each.with_object({}) do |name, hash|
+        hash[name] = Dojo.eager_load(:dojo_event_services).where(dojo_event_services: { name: name }).to_a
+      end
+    end
+
+    class Base
+      def initialize(dojos, from, to)
+        @externals = dojos[:externals]
+        @internals = dojos[:internals]
+        @list = build_list(from, to)
+        @from = from
+        @to = to
+      end
+
+      def run
+        with_notifying do
+          delete_event_histories
+          execute
+          execute_once
+        end
+      end
+
+      private
+
+      def with_notifying
+        yield
+        Notifier.notify_success(date_format(@from), date_format(@to))
+      rescue => e
+        Notifier.notify_failure(date_format(@from), date_format(@to), e)
+      end
+
+      ## TODO: modofy methods
+      def delete_event_histories
+        (@externals.keys + @internals.keys).each do |kind|
+          "Upcoming::Tasks::#{kind.to_s.camelize}".constantize.delete_event_histories(@from..@to)
+        end
+      end
+
+      def execute
+        raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
+      end
+
+      def execute_once
+        @internals.each do |kind, list|
+          "Upcoming::Tasks::#{kind.to_s.camelize}".constantize.new(list, nil, nil).run
+        end
+      end
+
+      def build_list(_from, _to)
+        raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
+      end
+
+      def date_format(_date)
+        raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
+      end
+    end
+
+    class Weekly < Base
+      private
+
+      def execute
+        @list.each do |date|
+          puts "Aggregate for #{date_format(date)}~#{date_format(date.end_of_week)}"
+
+          @externals.each do |kind, list|
+            "Statistics::Tasks::#{kind.to_s.camelize}".constantize.new(list, date, true).run
+          end
+        end
+      end
+
+      def build_list(from, to)
+        loop.with_object([from]) do |_, list|
+          nw = list.last.next_week
+          raise StopIteration if nw > to
+          list << nw
+        end
+      end
+
+      def date_format(date)
+        date.strftime('%Y/%m/%d')
+      end
+    end
+
+    class Monthly < Base
+      private
+
+      def execute
+        @list.each do |date|
+          puts "Aggregate for #{date_format(date)}"
+
+          @externals.each do |kind, list|
+            "Statistics::Tasks::#{kind.to_s.camelize}".constantize.new(list, date, false).run
+          end
+        end
+      end
+
+      def build_list(from, to)
+        loop.with_object([from]) do |_, list|
+          nm = list.last.next_month
+          raise StopIteration if nm > to
+          list << nm
+        end
+      end
+
+      def date_format(date)
+        date.strftime('%Y/%m')
+      end
+    end
+
+    class Notifier
+      class << self
+        def notify_success(from, to)
+          notify("#{from}~#{to}のイベント履歴の集計を行いました")
+        end
+
+        def notify_failure(from, to, exception)
+          notify("#{from}~#{to}のイベント履歴の集計でエラーが発生しました\n#{exception.message}\n#{exception.backtrace.join("\n")}")
+        end
+
+        private
+
+        def idobata_hook_url
+          return @idobata_hook_url if defined?(@idobata_hook_url)
+          @idobata_hook_url = ENV['IDOBATA_HOOK_URL']
+        end
+
+        def notifierable?
+          idobata_hook_url.present?
+        end
+
+        def notify(msg)
+          $stdout.puts msg
+          puts `curl --data-urlencode "source=#{msg}" -s #{idobata_hook_url} -o /dev/null -w "idobata: %{http_code}"` if notifierable?
+        end
+      end
+    end
+  end
+end
