822aeadb523dac56d3ab33a673d2ac1c5706db2d
Yohei Yasukawa <yasulab@gmail.com>
2018-06-07 11:54:02 +0900

    Fix wrong dojo count in Stats controller

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,7 @@
 class StatsController < ApplicationController
   def show
     @url                 = request.url
-    @dojo_count          = Dojo.count
+    @dojo_count          = Dojo.active.count
     @regions_and_dojos   = Dojo.group_by_region_on_active
 
     # TODO: 次の静的なDojoの開催数もデータベース上で集計できるようにする
