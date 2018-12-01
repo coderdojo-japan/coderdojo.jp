ebb2038ca0fee21fcb84d786cf335dc6422aac0d
Yohei Yasukawa <yohei@yasslab.jp>
2018-12-01 15:24:46 +0900

    Enable BASIC Auth for CoderDojo Spaces :secret:

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,5 +1,5 @@
 class SpacesController < ApplicationController
-  # http_basic_authenticate_with name: ENV['SPACES_BASIC_AUTH_NAME'], password: ENV['SPACES_BASIC_AUTH_PASSWORD'] if Rails.env.production?
+  http_basic_authenticate_with name: ENV['SPACES_BASIC_AUTH_NAME'], password: ENV['SPACES_BASIC_AUTH_PASSWORD'] if Rails.env.production?
 
   def index
     @dojo_count        = Dojo.count
