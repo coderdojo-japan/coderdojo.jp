ee2140c163698885285d34c2e9186555f80bae95
Yohei Yasukawa <yohei@yasslab.jp>
2019-03-29 17:42:19 +0900

    Push feed routing on the Rails way :dash:

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -2,9 +2,20 @@ class PodcastsController < ApplicationController
   def index
     @title = 'DojoCast'
     @desc  = 'Highlight people around CoderDojo communities by Podcast 📻✨'
-    @episodes = Podcast.all.sort_by{|episode| episode.filename.rjust(3, '0')}
+    @episodes = Podcast.all.sort_by{|episode| episode.published_at }
     @url      = request.url
     @next_live_date = ENV['NEXT_LIVE_DATE'] || '未定'
+
+    # For .rss format
+    @art_work_url = "https://coderdojo.jp/podcasts/cover.jpg"
+    @author       = "一般社団法人 CoderDojo Japan"
+    @copyright    = "Copyright © 2012-#{Time.current.year} #{@author}"
+    @base_url     = request.base_url
+    respond_to do |format|
+      format.html
+      format.rss  { render "feed", :layout => false }
+    end
+
   end
 
   def show
@@ -16,17 +27,4 @@ class PodcastsController < ApplicationController
     @url      = request.url
   end
 
-  def feed
-    @title        = "DojoCast"
-    @description  = "Highlight people around CoderDojo communities by Podcast."
-    @art_work_url = "https://coderdojo.jp/podcasts/cover.jpg"
-    @author       = "一般社団法人 CoderDojo Japan"
-    @copyright    = "Copyright © 2012-#{Time.current.year} #{@author}"
-
-    @episodes     = Podcast.all.sort_by{|episode| episode.published_at}
-    @base_url     = request.base_url
-    respond_to do |format|
-      format.rss { render :layout => false }
-    end
-  end
 end
