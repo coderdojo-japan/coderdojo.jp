1886f3967e1e0030def0a5adbf69938841c9002b
Chikako OHNO <chicaco.ohno@gmail.com>
2019-03-31 22:34:26 +0900

    migrate 後の schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180604070534) do
+ActiveRecord::Schema.define(version: 20190305145900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20180604070534) do
     t.datetime "updated_at", null: false
     t.integer "prefecture_id"
     t.boolean "is_active", default: true, null: false
+    t.boolean "is_private", default: false, null: false
   end
 
   create_table "event_histories", id: :serial, force: :cascade do |t|
