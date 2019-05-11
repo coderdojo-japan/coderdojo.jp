27663cd25b9f1753000bbdcf2022edf0c01616f9
Yohei Yasukawa <yohei@yasslab.jp>
2019-05-11 12:35:37 +0900

    Fix spec to test wording in Top

--- spec/features/top_spec.rb
+++ spec/features/top_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Top", type: :feature do
     scenario "Each section should exist" do
       visit "/"
       expect(page).to have_title 'CoderDojo Japan'
-      expect(page).to have_text  '全国の道場'
+      expect(page).to have_text  '日本各地の道場'
       expect(page).to have_css   'section.partners_logo a[href]'
       expect(page).to have_text  '問い合わせ'
       expect(page).to have_text  '一般社団法人'
