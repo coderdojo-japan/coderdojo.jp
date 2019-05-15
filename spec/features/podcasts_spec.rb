df3c8585d1ffbdce3fc9e6e4000f3c7bc6f7ff4e
Chikako OHNO <chicaco.ohno@gmail.com>
2019-05-15 23:54:16 +0900

    RSpec修正、Travis CI でのエラー対応見直し

--- spec/features/podcasts_spec.rb
+++ spec/features/podcasts_spec.rb
@@ -1,26 +1,37 @@
 require 'rails_helper'
 
-RSpec.feature "Podcasts", type: :feature do
-  describe "GET documents" do
-    scenario "Podcast index should be exist" do
-      visit "/podcasts"
+RSpec.feature 'Podcasts', type: :feature do
+  describe 'GET documents' do
+    scenario 'Podcast index should be exist' do
+      visit '/podcasts'
       expect(page).to have_http_status(:success)
     end
 
-    scenario "Charter should be exist" do
-      visit "/podcasts/1"
+    scenario 'Charter should be exist' do
+      @soundcloud_track = create(:soundcloud_track)
+      allow(@soundcloud_track).to receive(:exists?) { true }
+      allow(@soundcloud_track).to receive(:exists?).with(offset: -1) { false }
+      allow(@soundcloud_track).to receive(:content) { "title\n収録日: 2019/05/10\n..." }
+      allow(SoundCloudTrack).to receive(:find_by).with(id: @soundcloud_track.id.to_s) { @soundcloud_track }
+
+      visit "/podcasts/#{@soundcloud_track.id}"
       target = '← 目次へ'
       expect(page).to have_http_status(:success)
-      expect(page).to have_link target, href: "/podcasts"
+      expect(page).to have_link target, href: '/podcasts'
       click_link target
       expect(page).to have_http_status(:success)
     end
 
-    scenario "Load doc file with absolute path" do
-      visit  "/podcasts/10"
+    scenario 'Load doc file with absolute path' do
+      @soundcloud_track = create(:soundcloud_track)
+      allow(@soundcloud_track).to receive(:exists?) { true }
+      allow(@soundcloud_track).to receive(:content) { "title\n収録日: 2019/05/10\n..." }
+      allow(SoundCloudTrack).to receive(:find_by).with(id: @soundcloud_track.id.to_s) { @soundcloud_track }
+
+      visit  "/podcasts/#{@soundcloud_track.id}"
       target = '目次へ戻る'
       expect(page).to have_http_status(:success)
-      expect(page).to have_link target, href: "/podcasts"
+      expect(page).to have_link target, href: '/podcasts'
       click_link target
       expect(page).to have_http_status(:success)
     end
