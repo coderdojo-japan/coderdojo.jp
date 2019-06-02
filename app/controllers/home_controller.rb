56a5c61a11e095ad16e3f5fb4fced5a5dea1431d
Chikako OHNO <chicaco.ohno@gmail.com>
2019-06-02 23:58:18 +0900

    TOPページには直近のイベントを掲載しないので戻す

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,5 @@ class HomeController < ApplicationController
   def show
     @dojo_count        = Dojo.active_dojos_count
     @regions_and_dojos = Dojo.group_by_region_on_active
-    @upcoming_events   = UpcomingEvent.group_by_date
   end
 end
