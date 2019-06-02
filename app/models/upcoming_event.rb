353bab297786cd6c6b07c1c071bf14e84d5f5626
Chikako OHNO <chicaco.ohno@gmail.com>
2019-06-02 18:43:29 +0900

    直近のイベント情報を表示する

--- app/models/upcoming_event.rb
+++ app/models/upcoming_event.rb
@@ -8,5 +8,24 @@ class UpcomingEvent < ApplicationRecord
   validates :participants, presence: true
 
   scope :for, ->(service) { where(dojo_event_service: DojoEventService.for(service)) }
+  scope :since, ->(date) { where('event_at >= ?', date.beginning_of_day) }
   scope :until, ->(date) { where('event_at < ?', date.beginning_of_day) }
+
+  class << self
+    def group_by_date
+      eager_load(dojo_event_service: { dojo: :prefecture }).since(Time.zone.today).
+        order(:event_at).map(&:catalog).group_by { |d| d[:event_date] }
+    end
+  end
+
+  def catalog
+    {
+      dojo_name:            dojo_event_service.dojo.name,
+      dojo_prefecture_name: dojo_event_service.dojo.prefecture.name,
+      event_title:          event_title,
+      event_url:            event_url,
+      event_at:             event_at,
+      event_date:           event_at.to_date
+    }
+  end
 end
