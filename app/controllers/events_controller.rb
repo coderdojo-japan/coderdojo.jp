f8fc6b5b94ad389ab93d8ae8d1ef864824fb5d7f
Yohei Yasukawa <yohei@yasslab.jp>
2019-06-26 20:59:25 +0900

    PoC: Provide json format API of Upcoming Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,5 +2,10 @@ class EventsController < ApplicationController
   def index
     @url             = request.url
     @upcoming_events = UpcomingEvent.group_by_prefecture_and_date
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @upcoming_events }
+    end
   end
 end
