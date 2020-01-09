3294172e01f3bb8aeab07329d09d25c0e1e559f1
Yohei Yasukawa <yohei@yasslab.jp>
2020-01-09 11:02:22 +0900

    Sort variabls in action in the same order of View sections

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,14 +1,6 @@
 class StatsController < ApplicationController
   def show
     @url                 = request.url
-    @dojo_count          = Dojo.active_dojos_count
-    @regions_and_dojos   = Dojo.group_by_region_on_active
-
-    # TODO: 次の静的なDojoの開催数もデータベース上で集計できるようにする
-    # https://github.com/coderdojo-japan/coderdojo.jp/issues/190
-    @sum_of_events       = EventHistory.count
-    @sum_of_dojos        = DojoEventService.count('DISTINCT dojo_id')
-    @sum_of_participants = EventHistory.sum(:participants)
 
     # 2012年1月1日〜2019年12月31日までの集計結果
     period        = Time.zone.local(2012).beginning_of_year..Time.zone.local(2019).end_of_year
@@ -21,5 +13,16 @@ class StatsController < ApplicationController
     @annual_dojos_chart           = stats.annual_dojos_chart
     @annual_event_histories_chart = stats.annual_event_histories_chart
     @annual_participants_chart    = stats.annual_participants_chart
+
+    # 最新データ
+    # TODO: 次の静的なDojoの開催数もデータベース上で集計できるようにする
+    # https://github.com/coderdojo-japan/coderdojo.jp/issues/190
+    @dojo_count          = Dojo.active_dojos_count
+    @sum_of_events       = EventHistory.count
+    @sum_of_dojos        = DojoEventService.count('DISTINCT dojo_id')
+    @sum_of_participants = EventHistory.sum(:participants)
+
+    # 日本各地の道場
+    @regions_and_dojos   = Dojo.group_by_region_on_active
   end
 end
