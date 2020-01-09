afd8ceedbf6fdb9f70cf49a140638208ab2e13a1
Yohei Yasukawa <yohei@yasslab.jp>
2020-01-09 21:11:54 +0900

    Refactor: Rename vars and remove needless codes in Stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -12,20 +12,34 @@ class StatsController < ApplicationController
     @annual_event_histories_chart = stats.annual_event_histories_chart
     @annual_participants_chart    = stats.annual_participants_chart
 
-    # 推移テーブル
-    @dojos        = stats.annual_sum_total_of_aggregatable_dojo
-    @events       = stats.annual_count_of_event_histories
-    @participants = stats.annual_sum_of_participants
-
     # 最新データ
-    # TODO: 次の静的なDojoの開催数もデータベース上で集計できるようにする
-    # https://github.com/coderdojo-japan/coderdojo.jp/issues/190
-    @dojo_count          = Dojo.active_dojos_count
+    @sum_of_dojos        = Dojo.active_dojos_count
     @sum_of_events       = EventHistory.count
-    @sum_of_dojos        = DojoEventService.count('DISTINCT dojo_id')
     @sum_of_participants = EventHistory.sum(:participants)
+    # TODO: 静的なDojoの開催数もデータベース上で集計できるようにする
+    # https://github.com/coderdojo-japan/coderdojo.jp/issues/190
+
+    # 集計方法と集計対象
+    @aggregated_dojos   = DojoEventService.count('DISTINCT dojo_id')
+    @annual_dojos_table = stats.annual_sum_total_of_aggregatable_dojo
 
     # 日本各地の道場
-    @regions_and_dojos   = Dojo.group_by_region_on_active
+    @regions_and_dojos = Dojo.group_by_region_on_active
   end
 end
+
+    # NOTE: テーブル表示したいときは次のように書く
+    # @annual_dojos_table        = stats.annual_sum_total_of_aggregatable_dojo
+    # @annual_events_table       = stats.annual_count_of_event_histories
+    # @annual_participants_table = stats.annual_sum_of_participants
+    #   %tr
+    #     %td 開催数
+    #     - @events.each_value do |num|
+    #       %td= num
+    #     /%td= @events.values.sum
+    #   %tr
+    #     %td 参加数
+    #     - @participants.each_value do |num|
+    #       %td= num
+    #     /%td= @participants.values.sum
+
