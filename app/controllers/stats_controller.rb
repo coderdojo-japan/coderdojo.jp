0254f486030fa91a0485b728043bc7d212b56447
Yohei Yasukawa <yohei@yasslab.jp>
2020-01-09 11:21:10 +0900

    Sort variabls in action in the same order of View sections

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -5,14 +5,17 @@ class StatsController < ApplicationController
     # 2012年1月1日〜2019年12月31日までの集計結果
     period        = Time.zone.local(2012).beginning_of_year..Time.zone.local(2019).end_of_year
     stats         = Stat.new(period)
-    @dojos        = stats.annual_sum_total_of_aggregatable_dojo
-    @events       = stats.annual_count_of_event_histories
-    @participants = stats.annual_sum_of_participants
 
+    # 推移グラフ
     @high_charts_globals          = HighChartsBuilder.global_options
     @annual_dojos_chart           = stats.annual_dojos_chart
     @annual_event_histories_chart = stats.annual_event_histories_chart
     @annual_participants_chart    = stats.annual_participants_chart
+
+    # 推移テーブル
+    @dojos        = stats.annual_sum_total_of_aggregatable_dojo
+    @events       = stats.annual_count_of_event_histories
+    @participants = stats.annual_sum_of_participants
 
     # 最新データ
     # TODO: 次の静的なDojoの開催数もデータベース上で集計できるようにする
