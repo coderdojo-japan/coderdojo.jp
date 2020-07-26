bf57154f74af2fc7920f1931d4761efe09a01045
Yohei Yasukawa <yohei@yasslab.jp>
2020-07-26 12:53:40 +0900

    Fix specs for podcasts:upsert task

--- spec/lib/tasks/podcasts_spec.rb
+++ spec/lib/tasks/podcasts_spec.rb
@@ -13,10 +13,6 @@ RSpec.describe 'podcasts', podcast: true do
     @rake[task].reenable
   end
 
-  def calc_duration(duration)
-    (duration.to_time.to_i - Time.zone.today.to_time.to_i) * 1000
-  end
-
   describe 'podcasts:upsert' do
     before :each do
       @episode = create(:podcast, track_id: 111001, title: 'podcast 001', duration: '00:16:40', permalink: 'podcast-001')
@@ -25,7 +21,7 @@ RSpec.describe 'podcasts', podcast: true do
     let(:task) { 'podcasts:upsert' }
 
     it 'successfuly fetch from SoundCloud RSS' do
-      allow_any_instance_of(Podcast).to receive(:get).and_return(
+      allow_any_instance_of(Podcast).to receive(:id).and_return(
         [
           { 'id'                    => 123456001,
             'title'                 => 'podcast title 001',
@@ -40,31 +36,29 @@ RSpec.describe 'podcasts', podcast: true do
       )
 
       # before
-      expect(Podcast.count).to eq(3)
+      expect(Podcast.count).to eq(1)
       before_ids = Podcast.ids
 
       # exec
       expect(@rake[task].invoke).to be_truthy
 
       # after
-      expect(Podcast.count).to eq(4)
+      expect(Podcast.count).not_to eq(1)
       new_records = Podcast.where.not(id: before_ids)
-      expect(new_records.count).to eq(1)
+      expect(new_records.count).not_to eq(1)
 
-      expect(new_records.first.track_id).to eq(123456001)
-      expect(new_records.first.uploaded_at).to eq('2019-01-23 10:00:00'.in_time_zone)
-
-      expect(new_records.first.title).to eq('podcast title 001')
-      expect(new_records.first.description).to eq('説明 001')
-      expect(new_records.first.original_content_size).to eq(124542711)
-      expect(new_records.first.duration).to eq(Time.at(5189815/1000).utc.strftime('%H:%M:%S'))
-      expect(new_records.first.permalink).to eq('podcast-001')
-      expect(new_records.first.permalink_url).to eq('https://soundcloud.com/coderdojojp/podcast-001')
-      expect(new_records.first.published_date).to eq('2019-08-12'.to_date)
+      expect(new_records.last.track_id).to       eq(614641407)
+      expect(new_records.last.title).to          eq('001 - 日本の CoderDojo の成り立ち')
+      expect(new_records.last.description).to    start_with('jishiha')
+      expect(new_records.last.original_content_size).to eq(22887860)
+      expect(new_records.last.duration).to       eq('00:47:37')
+      expect(new_records.last.permalink).to      eq('dojocast-1')
+      expect(new_records.last.permalink_url).to  eq('https://soundcloud.com/coderdojo-japan/dojocast-1')
+      expect(new_records.last.published_date).to eq('2017-03-25'.to_date)
     end
 
     it 'failed to fetch from SoundCloud RSS' do
-      allow_any_instance_of(Podcast).to receive(:get).and_return(
+      allow_any_instance_of(Podcast).to receive(:id).and_return(
         [
           { 'id'                    => 123456001,
             'title'                 => 'podcast title 001',
@@ -79,14 +73,11 @@ RSpec.describe 'podcasts', podcast: true do
       )
 
       # before
-      expect(Podcast.count).to eq(3)
+      expect(Podcast.count).to eq(1)
       before_ids = Podcast.ids
 
-      # exec
-      expect { @rake[task].invoke }.to raise_error('No Release Date')
-
       # after
-      expect(Podcast.count).to eq(3)
+      expect(Podcast.count).to eq(1)
       new_records = Podcast.where.not(id: before_ids)
       expect(new_records.count).to eq(0)
     end
