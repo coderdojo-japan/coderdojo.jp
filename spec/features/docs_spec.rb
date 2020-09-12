516cff53269b461a37454571e50d1f137a8c6e3c
Yohei Yasukawa <yohei@yasslab.jp>
2020-09-12 18:45:48 +0900

    Fix failed spec in CI

--- spec/features/docs_spec.rb
+++ spec/features/docs_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Docs", type: :feature do
     scenario "Document index should be exist" do
       visit docs_path
       expect(page).to have_http_status(:success)
-      expect(page).to have_css 'section.keiyaku a[href]', count: Document.all.count
+      expect(page).to have_css 'section.doc a[href]', count: Document.all.count
     end
     scenario "Charter should be exist" do
       visit doc_path('charter')
