f5a6368e4d9227bac326c5c6821fef4ff01f77d3
Yohei Yasukawa <yohei@yasslab.jp>
2020-09-22 12:40:48 +0900

    Fix failed spec on posts

--- spec/features/news_spec.rb
+++ spec/features/news_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 RSpec.feature "News", type: :feature, scrivito: true do
   describe "GET /news/2016/12/12/new-backend" do
     scenario "Title should be formatted" do
-      visit "/news/2016/12/12/new-backend"
-      expect(page).to have_title "CoderDojo Japan のバックエンドを更新しました - CoderDojo Japan"
+      visit "/docs/post-backend-update-history"
+      expect(page).to have_title "CoderDojo Japan のバックエンド刷新"
     end
   end
 end
