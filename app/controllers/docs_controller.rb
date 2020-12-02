402c21ce1bd92611cb5b69de2be880f9255a5040
Yohei Yasukawa <yohei@yasslab.jp>
2020-12-02 09:33:56 +0900

    Remove calendars from docs#index page

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -2,7 +2,7 @@ class DocsController < ApplicationController
   def index
     @title = 'CoderDojo Japan ドキュメント集'
     @desc  = 'CoderDojo に関する公式情報を本ページでまとめています。'
-    @docs  = Document.all
+    @docs  = Document.all.reject{|doc| doc.title.start_with? '📆 予定表'}
     @url   = request.url
   end
 
@@ -13,7 +13,6 @@ class DocsController < ApplicationController
       @doc.content.gsub! "{{ NUM_OF_JAPAN_DOJOS }}", Dojo.active_dojos_count.to_s
       @doc.content.gsub! "{{ NUM_OF_WORLD_DOJOS }}", Dojo::NUM_OF_WORLD_DOJOS
       @doc.content.gsub! "{{ NUM_OF_COUNTRIES }}",   Dojo::NUM_OF_COUNTRIES
-
     end
     @content = Kramdown::Document.new(@doc.content, input: 'GFM').to_html
     @url     = request.url
