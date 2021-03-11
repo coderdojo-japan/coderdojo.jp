d6cda67e571f932754d0c522ce82c0eeb6a1227b
Yohei Yasukawa <yohei@yasslab.jp>
2021-03-11 16:29:45 +0900

    Refactor code layout to Pokemon controller

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,20 +1,21 @@
 class PokemonsController < ApplicationController
-  if Rails.env.staging? || Rails.env.production?
-    http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME_FOR_POKEMON'],
-                                 password: ENV['BASIC_AUTH_PASSWORD_FOR_POKEMON']
-  end
+  http_basic_authenticate_with(
+    name:     ENV['BASIC_AUTH_NAME_FOR_POKEMON'],
+    password: ENV['BASIC_AUTH_PASSWORD_FOR_POKEMON']) unless Rails.env.development?
   before_action :download_key_present?, only: :download
+
+  EXPIRATION_SECONDS = 300 # 300 seconds = 5 minutes
 
   def new; end
 
   def create
     pokemon = Pokemon.create(
-      email: params[:email],
-      parent_name: params[:parent_name],
+      email:            params[:email],
+      parent_name:      params[:parent_name],
       participant_name: params[:participant_name],
-      dojo_name: params[:dojo_name],
-      presigned_url: generate_presigned_url,
-      download_key: SecureRandom.hex
+      dojo_name:        params[:dojo_name],
+      presigned_url:    generate_presigned_url,
+      download_key:     SecureRandom.hex
     )
     redirect_to pokemon_download_path(key: pokemon.download_key)
   end
@@ -30,10 +31,9 @@ class PokemonsController < ApplicationController
     signer = Aws::S3::Presigner.new
     signer.presigned_url(
       :get_object,
-      bucket: 'coderdojo-jp-pokemon',
-      key: 'pokemon-sozai.zip',
-      #expires_in: 3600 # 60min
-      expires_in: 300   #  5min
+      bucket:     'coderdojo-jp-pokemon',
+      key:        'pokemon-sozai.zip',
+      expires_in: EXPIRATION_SECONDS
     )
   end
 
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,6 +1,7 @@
 class Pokemon < ApplicationRecord
-  validates :email, presence: true
-  validates :parent_name, presence: true
+  validates :email,            presence: true
+  validates :parent_name,      presence: true
   validates :participant_name, presence: true
-  validates :dojo_name, presence: true
+  validates :dojo_name,        presence: true
+  validates :download_key,     presence: true
 end
