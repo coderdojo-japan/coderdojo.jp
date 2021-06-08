61536d8b72ec201f5247e219de3af9c9a246c4d1
Yohei Yasukawa <yohei@yasslab.jp>
2021-06-08 17:33:33 +0900

    Disable error handling for Scrivito #1269

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
     rescue_from Exception,                      with: :render_500
     rescue_from ActiveRecord::RecordNotFound,   with: :render_404
     rescue_from ActionController::RoutingError, with: :render_404
-    rescue_from Scrivito::ResourceNotFound,     with: :render_404
+    # rescue_from Scrivito::ResourceNotFound,     with: :render_404
   end
 
   private
