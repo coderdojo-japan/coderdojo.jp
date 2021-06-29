c627b65c7f4f11dd179f60c0d3c2a692f5573b04
Yohei Yasukawa <yohei@yasslab.jp>
2021-06-29 15:15:45 +0900

    Cosmetic change in EventHistory model

--- app/models/event_history.rb
+++ app/models/event_history.rb
@@ -1,12 +1,12 @@
 class EventHistory < ApplicationRecord
   belongs_to :dojo
 
-  validates :dojo_name, presence: true
+  validates :dojo_name,    presence: true
   validates :service_name, presence: true, uniqueness: { scope: :event_id  }
-  validates :event_id, presence: true
-  validates :event_url, presence: true, uniqueness: true, allow_nil: true
+  validates :event_id,     presence: true
+  validates :event_url,    presence: true, uniqueness: true, allow_nil: true
   validates :participants, presence: true
-  validates :evented_at, presence: true
+  validates :evented_at,   presence: true
 
   scope :for,   ->(service) { where(service_name: service) }
   scope :within, ->(period) { where(evented_at: period) }
