6ae55970b7c7587bf510c75d5da89c040771fa7d
Yohei Yasukawa <yohei@yasslab.jp>
2021-07-12 11:26:47 +0900

    Revert "Add /debug to test embedded Google slides from Mobile"
    This reverts commit a484df64b2b1458c8729232204dc7f2cc61d67cf.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,4 @@ class HomeController < ApplicationController
     @dojo_count        = Dojo.active_dojos_count
     @regions_and_dojos = Dojo.group_by_region_on_active
   end
-
-  def debug
-    @dojo_count        = Dojo.active_dojos_count
-    @regions_and_dojos = Dojo.group_by_region_on_active
-  end
 end
