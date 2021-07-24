6f0f4c71bdbf296a50c13929a1e2e2b26af2d3d2
Yohei Yasukawa <yohei@yasslab.jp>
2021-07-24 15:13:23 +0900

    Stop using 'track_id' in sample data for test

--- spec/factories/podcasts.rb
+++ spec/factories/podcasts.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :podcast, class: Podcast do
-    track_id       { 123 }
     title          { 'title' }
     description    { 'description' }
     content_size   { 0 }
     duration       { 0 }
     permalink      { 'title' }
-    permalink_url  { 'http://aaa.bbb/title' }
+    permalink_url  { 'https://aaa.bbb/title' }
+    enclosure_url  { 'https://ccc.ddd/title.mp3' }
     published_date { Time.zone.today }
   end
 end
