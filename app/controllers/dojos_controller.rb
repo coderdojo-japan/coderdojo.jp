f48e7eb421e23f247a9010b2ab5f164fa1c7f3da
Yohei Yasukawa <yohei@yasslab.jp>
2022-05-17 17:06:38 +0900

    Add internal Dojo API to make news easier
    下記お知らせ記事の「寄贈先の CoderDojo 一覧」を簡単に作りたい。
    与えられた Dojo 名と一致した Dojo のリンク付きテキストを返し、
    今後同様のセクションを作るときは本 API で Dojo 一覧のリストを用意する。
    
    「CoderDojo 一覧」が用意されているお知らせ記事の例
    https://news.coderdojo.jp/2022/05/16/1500-microbits-to-coderdojo/

---
+++ app/controllers/dojos_controller.rb
@@ -0,0 +1,21 @@
+class DojosController < ApplicationController
+  def index
+    @dojo_data = []
+    Dojo.active.each do |dojo|
+      @dojo_data << {
+        name: dojo.name,
+        url:  dojo.url,
+        prefecture: dojo.prefecture.region,
+        linked_text: "<a href='#{dojo.url}'>'#{dojo.name}</a>（#{dojo.prefecture.region}）",
+      }
+    end
+
+    respond_to do |format|
+      format.json  { render json: @dojo_data }
+
+      # No corresponding View for now.
+      # Only for API: GET /dojos.json
+      format.html { redirect_to root_url(anchor: 'dojos') }
+    end
+  end
+end
