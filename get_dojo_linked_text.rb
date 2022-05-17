39a4e925813becf508e32743f181b0e1913c4da3
Yohei Yasukawa <yohei@yasslab.jp>
2022-05-17 17:35:52 +0900

    Add comment to do it later #1433

--- get_dojo_linked_text.rb
+++ get_dojo_linked_text.rb
@@ -10,6 +10,8 @@ result   = "<ul>\n"
 #pp dojos
 #binding.pry
 
+# TODO: スプレッドシートの入力列（コピペ）と、
+#       API 取得結果を突き合わせる処理を追加する。
 dojos.sample(5).each do |d|
   result << "  <li>#{d['linked_text']}</li>\n"
 end
