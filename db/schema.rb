ecfd201d0d00d96d6f988d58b26ba5d5709dbd69
Yohei Yasukawa <yohei@yasslab.jp>
2023-03-18 20:38:01 +0900

    Add 'pg_stat_statements extention' to db/schema.rb
    This code is automatically generated by exec: bundle install

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2023_03_07_132540) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "dojo_event_services", id: :serial, force: :cascade do |t|
