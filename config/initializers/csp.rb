b4d409dcab112fe457dc14615a477163c20f4b55
Yohei Yasukawa <yohei@yasslab.jp>
2024-03-30 11:19:21 +0900

    Remove needless '*.unpkg.com' from CSP

--- config/initializers/csp.rb
+++ config/initializers/csp.rb
@@ -3,7 +3,7 @@ SecureHeaders::Configuration.default do |config|
            *.google-analytics.com *.googleapis.com *.google.com *.gstatic.com
            *.facebook.net *.facebook.com *.twitter.com *.mailchimp.com
            *.hatena.ne.jp *.st-hatena.com *.line-scdn.net *.mixlr.com
-           *.slidesharecdn.com *.slideshare.net *.unpkg.com *.jsdelivr.net)
+           *.slidesharecdn.com *.slideshare.net *.jsdelivr.net)
   config.csp = {
     report_only:      false,
     preserve_schemes: true, # default: false
