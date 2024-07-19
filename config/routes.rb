471cef29c47390a16025c9171f9385c481884915
Yohei Yasukawa <yohei@yasslab.jp>
2024-07-19 16:22:12 +0900

    Update link to the related Champions post

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
   get "/docs/_calendar-kirie",    to: redirect('/calendar/kirie')
   get "/docs/_thanks",            to: redirect('/thanks')
 
-  get "/redirects/202407",        to: redirect('https://www.facebook.com/groups/coderdojo.jp.champions')
+  get "/redirects/202407",        to: redirect('https://www.facebook.com/groups/coderdojo.jp.champions/posts/7788378511253707/')
   get "/youtube",                 to: redirect('https://www.youtube.com/CoderDojoJapan')
   get "/brand",                   to: redirect('/docs/brand-guidelines')
   get "/calendar",                to: redirect('/calendar/yohei')
