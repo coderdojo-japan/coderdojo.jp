7c5c5f7b1c6c6ac83fb3c3058d49da075774520d
Yohei Yasukawa <yohei@yasslab.jp>
2025-01-31 12:09:22 +0900

    Fix navigation letter to click in test suites

--- spec/features/podcasts_spec.rb
+++ spec/features/podcasts_spec.rb
@@ -15,9 +15,10 @@ RSpec.feature 'Podcasts', type: :feature do
       allow(Podcast).to  receive(:find_by).with(id: @podcast.id.to_s) { @podcast }
 
       visit "/podcasts/#{@podcast.id}"
-      target = '&laquo; Top'
       expect(page).to have_http_status(:success)
-      expect(page).to have_link target, href: '/podcasts'
+
+      target = 'Top'
+      expect(page).to have_link target, href: podcasts_path
       click_link target, match: :first
       expect(page).to have_http_status(:success)
     end
