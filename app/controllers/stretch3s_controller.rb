7c189a8b5c906fe382b154949503628b830a1e01
ichiroc <nobitahouse@gmail.com>
2025-03-16 00:37:33 +0900

    stretch3 の申請フォームを登録して ChatGPT2Scratch を利用できるように

---
+++ app/controllers/stretch3s_controller.rb
@@ -0,0 +1,21 @@
+class Stretch3sController < ApplicationController
+  def new
+    @stretch3 = Stretch3.new
+  end
+
+  def create
+    @stretch3 = Stretch3.new(stretch3_params)
+    if @stretch3.save
+      # NOTE: coderdojo.jp ドメインから遷移すると Stretch3 側で ChatGPT2Scratch の API キーが不要になる処理が走る
+      redirect_to 'https://stretch3.github.io', allow_other_host: true
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def stretch3_params
+    params.require(:stretch3).permit(:email, :parent_name, :participant_name, :dojo_name)
+  end
+end
