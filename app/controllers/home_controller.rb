d75a1be1c7f6a31bbecc1e8738f126995bad841a
nacchan <spring.moon.0421@gmail.com>
2025-06-10 09:06:47 +0900

    home_controller.rb のインデントを統一（L5〜L11を一段下げ）

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
   def show
-  @dojo_count = Dojo.active_dojos_count
-  @regions_and_dojos = Dojo.group_by_region_on_active
-  @prefectures_and_dojos = Dojo.group_by_prefecture_on_active.transform_values do |dojos|
-    count = dojos.pluck(:counter).sum
-    {
-      dojos: dojos,
-      label: "#{count} Dojo#{'s' if count != 1}"
-    }
+    @dojo_count        = Dojo.active_dojos_count
+    @regions_and_dojos = Dojo.group_by_region_on_active
+    @prefectures_and_dojos = Dojo.group_by_prefecture_on_active.transform_values do |dojos|
+      count = dojos.pluck(:counter).sum
+      {
+        dojos: dojos,
+        label: "#{count} Dojo#{'s' if count != 1}"
+      }
     end
   end
 end
