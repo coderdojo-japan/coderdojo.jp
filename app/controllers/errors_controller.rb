7e4737c6e9739812696784377e3b6f4c75729918
nacchan <spring.moon.0421@gmail.com>
2025-06-12 07:58:24 +0900

    コメント位置を上に移動して統一

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,14 +2,17 @@ class ErrorsController < ApplicationController
   layout 'application'
 
   def not_found
-    render status: 404 # このアクションでは app/views/errors/not_found.html.erb が使用されます
+    # このアクションでは app/views/errors/not_found.html.erb が使用されます
+    render status: 404
   end
 
   def internal_server_error
-    render status: 500 # このアクションでは app/views/errors/internal_server_error.html.erb が使用されます
+    # このアクションでは app/views/errors/internal_server_error.html.erb が使用されます
+    render status: 500
   end
 
   def unprocessable_entity
-    render status: 422 # このアクションでは app/views/errors/unprocessable_entity.html.erb が使用されます
+    # このアクションでは app/views/errors/unprocessable_entity.html.erb が使用されます
+    render status: 422
   end
 end
