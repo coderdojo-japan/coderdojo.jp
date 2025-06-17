5a6566d98e156872fa89f9c87730239e64926f3c
nacchan <spring.moon.0421@gmail.com>
2025-06-17 11:51:48 +0900

    エラー表示をヘルパー化
    ・ErrorsHelperにerror_title/error_descメソッドを追加
    ・local_assigns[:status_code]をもとに@title/@descをセット
    ・provideでレイアウト用のタイトル・ディスクリプションを連携

---
+++ app/helpers/errors_helper.rb
@@ -0,0 +1,19 @@
+module ErrorsHelper
+  def error_title(code)
+    case code
+    when 404 then "ページが見つかりませんでした... 🥺💦"
+    when 422 then "リクエストが処理できませんでした… 😢"
+    when 500 then "予期しないエラーが発生しました 😵‍💫"
+    else           "予期せぬエラーが発生しました…😵"
+    end
+  end
+
+  def error_desc(code)
+    case code
+    when 404 then "ページが削除された可能性があります 🤔💭"
+    when 422 then "入力内容に誤りがあるか、リクエストが正しく送信されなかった可能性があります。"
+    when 500 then "申し訳ありません。サーバーで問題が発生しています。"
+    else           "しばらく経ってから再度お試しください。"
+    end
+  end
+end
