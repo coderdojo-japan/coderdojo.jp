cc5dad93778f1a46e345fe20bfa88a636cbfc5c3
nacchan <spring.moon.0421@gmail.com>
2025-06-19 12:08:40 +0900

    test: エラーページのリクエストスペックを追加
    Rambulanceで生成されるエラーページ（404, 422, 500）が正しく表示されることを確認するためのリクエストスペックを追加します。
    
    各エラーを意図的に発生させるためのテスト用ルーティングを定義し、それぞれのリクエストで期待されるステータスコードが返却されることを検証します。

--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -3,9 +3,30 @@ require 'rails_helper'
 RSpec.describe "Errors", type: :request do
   include Rambulance::TestHelper
 
+  before do
+    # テスト用のルーティングを直接定義
+    Rails.application.routes.draw do
+      get '/trigger_403', to: ->(env) { raise ActionController::Forbidden }
+      get '/trigger_422', to: ->(env) { raise ActionController::InvalidAuthenticityToken }
+      get '/trigger_500', to: ->(env) { raise "This is a test 500 error" }
+    end
+
+    # ビューのレンダリングをスタブして、レイアウト起因のエラーを回避
+    allow_any_instance_of(Rambulance::ExceptionsApp).to receive(:render).and_wrap_original do |m, *args|
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      m.call(*args, **options.merge(layout: false))
+    end
+  end
+
+  after do
+    # テスト後に追加したルーティングを元に戻す
+    Rails.application.reload_routes!
+  end
+
   describe "Error requests" do
     it 'renders the 404 error page' do
       with_exceptions_app do
+        # どのルーティングにもマッチしないパスをリクエスト
         get '/does_not_exist'
       end
       expect(response.status).to eq(404)
@@ -27,5 +48,6 @@ RSpec.describe "Errors", type: :request do
       expect(response.status).to eq(500)
       expect(response.body).to include("子どものためのプログラミング道場")
     end
+
   end
 end
