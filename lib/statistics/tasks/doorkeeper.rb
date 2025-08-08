d14608feb9005de0fb26cb3bea5cb5ca0eb83ce9
Yohei Yasukawa <yohei@yasslab.jp>
2025-08-08 19:46:14 +0900

    fix: Doorkeeperイベントデータのキーをシンボルに統一
    Statistics::Tasks::Doorkeeperで文字列キーでアクセスしていたが、
    EventService::Providers::Doorkeeperがシンボルキーで返すように
    変更されたため、統計集計でDoorkeeperイベントが作成されない問題を修正。
    
    また、テストスタブの日付を動的に生成するように変更し、
    期間に応じた正しいイベント数が作成されるように調整した。

--- lib/statistics/tasks/doorkeeper.rb
+++ lib/statistics/tasks/doorkeeper.rb
@@ -20,16 +20,16 @@ module Statistics
           dojo.dojo_event_services.for(:doorkeeper).each do |dojo_event_service|
             events = @client.fetch_events(**@params.merge(group_id: dojo_event_service.group_id))
             (events || []).compact.each do |e|
-              next unless e['group'].to_s == dojo_event_service.group_id
+              next unless e[:group].to_s == dojo_event_service.group_id
 
               EventHistory.create!(dojo_id:          dojo.id,
                                    dojo_name:        dojo.name,
                                    service_name:     dojo_event_service.name,
                                    service_group_id: dojo_event_service.group_id,
-                                   event_id:     e['id'],
-                                   event_url:    e['public_url'],
-                                   participants: e['participants'],
-                                   evented_at:   Time.zone.parse(e['starts_at']))
+                                   event_id:     e[:id],
+                                   event_url:    e[:public_url],
+                                   participants: e[:participants],
+                                   evented_at:   Time.zone.parse(e[:starts_at]))
             end
           end
         end
