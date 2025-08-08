56e44ba8bb99dde1ebc7e1e66b37ca828689617b
Yohei Yasukawa <yohei@yasslab.jp>
2025-08-08 15:45:47 +0900

    style: 日付表示を自然な日本語に改善
    現在年の表示を「2025年08月08日時点」から「2025年8月8日時点」に変更。
    日本語として自然な表記（ゼロパディングなし）を採用。

--- app/controllers/dojos_controller.rb
+++ app/controllers/dojos_controller.rb
@@ -69,7 +69,7 @@ class DojosController < ApplicationController
       
       # 表示用の日付テキスト
       display_date = "#{@selected_year}年末"
-      display_date = Date.current.strftime('%Y年%m月%d日') if @selected_year == Date.current.year
+      display_date = Date.current.strftime('%Y年%-m月%-d日') if @selected_year == Date.current.year
       
       flash.now[:inline_info] = "#{display_date}時点のアクティブな道場を表示中<br>（開設数: #{new_dojos_count} / 合計数: #{total_dojos_count}）".html_safe
     end
