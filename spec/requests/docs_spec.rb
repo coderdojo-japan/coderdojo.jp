6ccf83a078b792c9471647b0e0e993cba0bf2573
Yohei Yasukawa <yohei@yasslab.jp>
2025-08-16 17:45:00 +0900

    style: クォートスタイルをシングルクォートに統一
    RuboCopの推奨に従い、文字列リテラルをダブルクォートから
    シングルクォートに変更（Rubyコミュニティの標準的なスタイル）

--- spec/requests/docs_spec.rb
+++ spec/requests/docs_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Docs", type: :request do
-  describe "GET /docs" do
-    it "show @docs" do
+RSpec.describe 'Docs', type: :request do
+  describe 'GET /docs' do
+    it 'show @docs' do
       get docs_path
       expect(response.body).to include(Document.all.last.title)
     end
   end
 
-  describe "GET /docs/:id" do
-    it "show @content" do
-      param    = 'charter'
+  describe 'GET /docs/:id' do
+    it 'show @content' do
+      param = 'charter'
       get doc_path(param)
       doc      = Document.new(param)
       expected = Kramdown::Document.new(doc.content, input: 'GFM').to_html
