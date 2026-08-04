03283e1dd1b34480e9318ae1a6443ceb89895c3c
Yohei Yasukawa <yohei@yasslab.jp>
2026-08-04 19:32:21 +0900

    docs(migration): null 許容にした経緯として PR へのリンクを残す

--- db/migrate/20260804101616_add_global_club_id_to_dojos.rb
+++ db/migrate/20260804101616_add_global_club_id_to_dojos.rb
@@ -6,6 +6,10 @@ class AddGlobalClubIdToDojos < ActiveRecord::Migration[8.0]
     # null 許容にしている理由: 閉鎖などで Clubs API 側に存在しない Dojo が
     # 現時点で 76 件あり、すべてを埋めることはできないため。
     # PostgreSQL のユニークインデックスは NULL を複数許すので共存できる。
+    #
+    # 設計文書 (PR #1747) が指定する null: false を見送った経緯や、
+    # 初期データの投入方法は PR #1867 を参照。
+    # https://github.com/coderdojo-japan/coderdojo.jp/pull/1867
     add_column :dojos, :global_club_id, :string
     add_index  :dojos, :global_club_id, unique: true
   end
