56ed83a6d09c5c911710d1c022c7292fdef12b23
Yohei Yasukawa <yohei@yasslab.jp>
2026-09-07 22:41:11 +0900

    fix(helpers): Host が非 ASCII のときにページ全体が 500 になるのを直す (#1912)
    og:url に Host 由来の値をそのまま入れており、Host ヘッダに非 ASCII の
    バイト列が含まれるとレイアウトの描画で落ちていた。
    
      ActionView::Template::Error: incompatible character encodings: UTF-8 and BINARY
        app/views/layouts/application.html.erb:21
    
    Puma が env に入れる HTTP_HOST は ASCII-8BIT で、request.url も *_url
    ヘルパーもそのエンコーディングを引き継ぐ。UTF-8 のビューに結合すると落ちる。
    
    入口は request.url だけではない。コントローラの @url、ビューの dojos_url
    などから来る値も同じで、最初は request.url だけを直して /kata しか
    直っていなかった (8 ページ中 7 ページが落ちたまま)。全部が full_url を
    通るので、そこで一度だけ落とす。
    
    このアプリは config.hosts を設定しておらず Host を検証しない。検証するか
    どうかは方針の判断なので別に扱う。存在しない /docs/xxx へのリダイレクトが
    OpenRedirectError になる経路も、Host の正当性を見るものなので残している。

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,4 +3,46 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   # News関連のメソッドはNewsモデルに移動しました
   # spec/models/news_spec.rb を参照
+
+  # og:url と Twitter カードの URL を組み立てる。
+  #
+  # == 経緯（2026/09/07）==
+  # Host ヘッダに非 ASCII のバイト列が入ると 500 になっていた。
+  #
+  #   ActionView::Template::Error: incompatible character encodings: UTF-8 and BINARY
+  #     app/views/layouts/application.html.erb:21
+  #
+  # Puma が env に入れる HTTP_HOST は ASCII-8BIT で、request.url もその
+  # エンコーディングを引き継ぐ。それを UTF-8 のビューに結合すると落ちる。
+  # このアプリは config.hosts を設定しておらず、Host は検証されない。
+  describe '#full_url' do
+    context '引数が空のとき' do
+      it 'リクエストの URL を返す' do
+        allow(helper).to receive(:request).and_return(double(url: 'https://coderdojo.jp/kata'))
+        expect(helper.full_url('')).to eq 'https://coderdojo.jp/kata'
+      end
+
+      it '非 UTF-8 のバイト列を含んでいても、ビューに埋め込める文字列を返す' do
+        broken = "https://www.coderdojo.jp\xFF/kata".dup.force_encoding(Encoding::ASCII_8BIT)
+        allow(helper).to receive(:request).and_return(double(url: broken))
+
+        result = helper.full_url('')
+        expect(result.encoding).to eq Encoding::UTF_8
+        expect(result).to be_valid_encoding
+        expect { +'' << result }.not_to raise_error
+      end
+    end
+
+    context '引数がパスのとき' do
+      it '本番のホストを補って返す' do
+        expect(helper.full_url('/kata')).to eq 'https://coderdojo.jp/kata'
+      end
+    end
+
+    context '引数が URL のとき' do
+      it 'そのまま返す' do
+        expect(helper.full_url('https://news.coderdojo.jp/')).to eq 'https://news.coderdojo.jp/'
+      end
+    end
+  end
 end
